762dfe996afbb0818e2f5b4949c154c8d5e185c6
Maneesh Dhagat <mdhagat@yahoo.com>
2013-10-15 17:06:35 -0700

    Added controllers sessions and wizards

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,8 @@ Planner::Application.routes.draw do
   resources :profiles
 
   resources :users
+  
+  resource :session
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
